96c27f67869b9a37ce1ebfffbed3f71ca715144a
EzioAARM <alejandrom9712@gmail.com>
2018-10-07 22:56:35 -0600

    Actualización de diseño

--- app/src/main/java/com/ed2/aleja/cifrado/ZigZagFragment.java
+++ app/src/main/java/com/ed2/aleja/cifrado/ZigZagFragment.java
@@ -1,14 +1,19 @@
 package com.ed2.aleja.cifrado;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class ZigZagFragment extends Fragment {
 
+    private static final int READ_REQUEST_CODE = 42;
     View rootView;
 
     @Nullable
@@ -16,6 +21,26 @@ public class ZigZagFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.zigzag_fragment, container, false);
 
+        Button cifrar = (Button) rootView.findViewById(R.id.examinar_cifrar_zigzag);
+        cifrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent()
+                        .addCategory(Intent.CATEGORY_OPENABLE)
+                        .setType("*/*")
+                        .setAction(Intent.ACTION_OPEN_DOCUMENT);
+                startActivityForResult(intent, READ_REQUEST_CODE);
+            }
+        });
+
         return rootView;
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            Toast.makeText(rootView.getContext(), "Si se seleecionó algo", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(rootView.getContext(), "No se seleccionón ningún archivo", Toast.LENGTH_SHORT).show();
+        }
     }
 }
