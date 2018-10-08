9bb415e9d4b1cd870980f2d227e457f0e4ca482b
EzioAARM <alejandrom9712@gmail.com>
2018-10-07 22:11:05 -0600

    Actualización de diseño

--- app/src/main/java/com/ed2/aleja/cifrado/CifrarFragment.java
+++ app/src/main/java/com/ed2/aleja/cifrado/CifrarFragment.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 public class CifrarFragment extends Fragment {
     View rootView;
@@ -15,6 +18,26 @@ public class CifrarFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.cifrar_fragment, container, false);
+
+        // Mostrar opciones de cifrado
+        Spinner metodosCifrado = rootView.findViewById(R.id.cifrados_cifrar);
+        metodosCifrado.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                        fragmentManager.beginTransaction().replace(R.id.contenedor_cifrados, new ZigZagFragment()).commit();
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                parent.setSelection(0);
+            }
+        });
+
         return rootView;
     }
 }
